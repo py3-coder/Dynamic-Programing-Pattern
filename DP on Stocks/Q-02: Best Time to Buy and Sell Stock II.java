82d2f9bdf1646446a2c86126290113588a1a465c
Saurabh Kumar <54509629+py3-coder@users.noreply.github.com>
2024-05-29 18:18:13 +0530

    Update Q-02: Best Time to Buy and Sell Stock II.java

--- DP on Stocks/Q-02: Best Time to Buy and Sell Stock II.java
+++ DP on Stocks/Q-02: Best Time to Buy and Sell Stock II.java
@@ -28,63 +28,69 @@ Constraints:
 0 <= prices[i] <= 104
 */
 class Solution {
-    static int memo[][] =new int[30001][2];
+    static int[][] memo ;
     public int maxProfit(int[] prices) {
-        //Recursion::)
+        int n = prices.length;
+        memo = new int[n][2];
+        Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
         //return solveRec(prices,0,1);
-
-        // Arrays.stream(memo).forEach(a->Arrays.fill(a,-1));
-        // return solveMemo(prices,0,1);
-
-        return solveTab(prices);
+        //return solveTab(prices);
         
+        return solveOpt(prices);
     }
-    public static int solveRec(int[] prices ,int indx,int buy){
-        //Base Case ::
-        if(indx==prices.length) return 0;
-        int profit=0;
-        if(buy==1){
-            profit = -prices[indx]+solveRec(prices,indx+1,0);
-        }else{
-            profit =prices[indx]+solveRec(prices,indx+1,1);
+    public int solveRec(int[] prices , int indx , int buy){
+        //Base Case :-
+        if(indx >= prices.length){
+            return  0;
         }
-        int skip =solveRec(prices,indx+1,buy);
-        return Math.max(profit,skip);
-    }
-    public static int solveMemo(int[] prices,int indx,int buy){
-        //Base Case ::
-        if(indx==prices.length) return 0;
         if(memo[indx][buy]!=-1){
             return memo[indx][buy];
         }
-        int profit=0;
+        int profit =0;
         if(buy==1){
-            profit = -prices[indx]+solveMemo(prices,indx+1,0);
+            profit = -prices[indx]+ solveRec(prices,indx+1,0);
         }else{
-            profit =prices[indx]+solveMemo(prices,indx+1,1);
+            profit = prices[indx] + solveRec(prices,indx+1,1);
         }
-        int skip =solveMemo(prices,indx+1,buy);
-        return memo[indx][buy]=Math.max(profit,skip);
+        int skip = solveRec(prices,indx+1,buy);
+        return memo[indx][buy] = Math.max(profit,skip);
     }
-    public static int solveTab(int[] prices){
-        int n=prices.length;
-        int tab[][] =new int[n+1][2];
-        Arrays.stream(tab).forEach(a->Arrays.fill(a,-1));
-        tab[n][0]=0;
-        tab[n][1]=0;
-
-        int profit=0;
-        for(int i=n-1;i>=0;i--){
-            for(int j=0;j<=1;j++){
-                if(j==1){
-                    profit = Math.max(prices[i]+tab[i+1][0],tab[i+1][1]);
+    public int solveTab(int[] prices){
+        int n = prices.length;
+        int[][] tab = new int[n+1][2];
+        
+        for(int indx = n-1 ;indx>=0 ;indx--){
+            for(int buy =0;buy<2;buy++){
+                int profit =0;
+                if(buy==1){
+                    profit = -prices[indx]+ tab[indx+1][0];
+                }else{
+                    profit = prices[indx] + tab[indx+1][1];
                 }
-                if(j==0){
-                    profit = Math.max(-prices[i]+tab[i+1][1],tab[i+1][0]);
-                }
-                tab[i][j]=profit;
+                int skip =tab[indx+1][buy];
+                tab[indx][buy] = Math.max(profit,skip);
             }
-        }  
-        return tab[0][0];      
+        }
+        return tab[0][1];
+    }
+    public int solveOpt(int[] prices){
+        int n = prices.length;
+        
+        int[] curr = new int[2];
+        int[] next =new int[2];
+        for(int indx = n-1 ;indx>=0 ;indx--){
+            for(int buy =0;buy<2;buy++){
+                int profit =0;
+                if(buy==1){
+                    profit = -prices[indx]+ next[0];
+                }else{
+                    profit = prices[indx] + next[1];
+                }
+                int skip =next[buy];
+                curr[buy] = Math.max(profit,skip);
+            }
+            next = curr ;
+        }
+        return curr[1];
     }
 }
